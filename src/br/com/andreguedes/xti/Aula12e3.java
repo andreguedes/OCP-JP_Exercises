d5a8376b71085d1e73ffa84e39f416ffd38fb85d
andresguedes <andresguedes@live.com>
2016-02-14 00:44:55 -0200

    Update in files, adding the lesson 004 - How to compile and run a Java file

---
+++ src/br/com/andreguedes/xti/Aula12e3.java
@@ -0,0 +1,13 @@
+package br.com.andreguedes.xti;
+
+public class Aula12e3 {
+	
+	// Aula 001 - Introducao
+	// Aula 002 - Hardware e Software
+	// Aula 003 - Instalacao e configuracao do Java
+	
+	public static void main(String[] args) {
+		System.out.println("Lessons 01, 02 e 03 - Introduction to course of Java Certification");
+	}
+
+}
---
+++ src/br/com/andreguedes/xti/Aula4.java
@@ -0,0 +1,27 @@
+package br.com.andreguedes.xti;
+
+/**
+ * public - Access modifier
+ * class - Java type
+ * Aula4 - Class name
+ * @author Andre
+ *
+ */
+public class Aula4 {
+	// Class scope
+
+	// Aula 004 - Hello World
+	public static void main(String[] args) {
+		// Method scope
+		
+		System.out.println("Hello World!");
+		
+		/*
+		 * To compile .java file using the prompt command, 
+		 * we should use the command "javac <file_name>.java";
+		 * 
+		 * To run the program, use "java <file_name>" without ".class"
+		 */
+	}
+	
+}
