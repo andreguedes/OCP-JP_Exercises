6a10b28e64bc792861afdd08c11a990e0daf1174
andresguedes <andresguedes@live.com>
2016-02-15 23:09:12 -0200

    Adding the Lesson 5 about fundamentals, compile errors, scape characters and conventions

---
+++ src/br/com/andreguedes/xti/Aula5.java
@@ -0,0 +1,46 @@
+package br.com.andreguedes.xti;
+
+/**
+ * Multiple line comment
+ * @author Andre
+ *
+ */
+/*
+ * Multiple line comment without specific informations
+ */
+// Only one line comment
+public class Aula5 {
+
+// This will not compile	
+//public class 5Aula5 {
+	
+	// Aula 005 - Fundamentos Java
+	public static void main(String[] args) {
+		
+	// This will not compile	
+	//public static void MAIN(String[] args) {
+		
+		System.out.print("\tHello Comments!!");
+		System.out.println("Java SE!!");
+	}
+	
+	// Conventions
+	/*
+	 * The class name must be equal the Java file name
+	 * The name of class should get the camel-case convention and could use (_, $),
+	 * but it is not a best practice for the class names
+	 * 
+	 * Use can`t reserved word for names.
+	 * e.g.: public class class
+	 * 
+	 * The name of the class, must be represent a object, so, it must be a noun
+	 */
+	
+	// Scape character
+	/* \t - TAB
+	 * \n - New line
+	 * \" - Show "
+	 * \\ - Show \
+	 */
+
+}
