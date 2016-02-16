709b0074d62de701160b8f77c3c74b58dd3d11e2
andresguedes <andresguedes@live.com>
2016-02-16 13:08:47 -0200

    Adding the Lessons 6 and 7 - About variables and primitive types

---
+++ src/br/com/andreguedes/xti/Aula6.java
@@ -0,0 +1,24 @@
+package br.com.andreguedes.xti;
+
+/**
+ * Learning variable concepts
+ * @author Andre
+ *
+ */
+public class Aula6 {
+	// Aula 006 - Variaveis (Sintaxes e nomenclaturas)
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * String - type
+		 * = - Operator
+		 */
+		String name = "Andre"; // String name - null
+		String _middleName, $surName; // Valid characters for attributes
+		int age = 27; // Or int age - 0
+		boolean married = false; // Or boolean married - false;
+		
+		System.out.println(name);
+	}
+}
---
+++ src/br/com/andreguedes/xti/Aula7.java
@@ -0,0 +1,47 @@
+package br.com.andreguedes.xti;
+
+public class Aula7 {
+
+	// Aula 007 - Tipos primitivos
+	public static void main(String[] args) {
+		
+		// Primitive types
+		/*
+		 * int - value without fraction - 4 bytes - MAX 2.147.483.647
+		 * long - large number without fraction - MAX 9.000.000.000.000.000.000
+		 * char - a character, and only one, within ''
+		 * byte - short number without fraction - MAX 127
+		 * float - number with fraction
+		 * double - number with fraction
+		 * short - short number without fraction - MAX 32767
+		 * void - without value
+		 * boolean - true or false (conditional)
+		 */
+		int age = 2_130_566_456;
+		double price = 2.56;
+		char n = 'n'; // Unicode
+		boolean married = false;
+		byte b = 127;
+		short s = 32767;
+		long l = 9_000_000_000_000_000_000L;
+		double d = 1.7975653843876623E+308; // IEEE 754
+		float f = 123F;
+		
+		// Implicit casting
+		age = s;
+		System.out.println(age);
+		
+		// Explicit casting
+		age = (int) l;
+		System.out.println(age);
+		
+		// Binary form
+		byte bb = 0b01010101; // 1 byte - 8 bits
+		short sb = 0b0101010101010101; // 2 bytes - 16 bits
+		int ib = 0b01010101010101010101010101010101; // 4 bytes - 32 bits
+		
+		System.out.println(ib);
+		
+		// A primitive type isn`t a object, it`s only your own value
+	}
+}
