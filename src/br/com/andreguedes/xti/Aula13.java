294cfc2d59e5058561a7d3238418fe1e04b39e83
andresguedes <andresguedes@live.com>
2016-03-03 18:57:42 -0300

    Adding the Lesson 13 - Operands and operators

---
+++ src/br/com/andreguedes/xti/Aula13.java
@@ -0,0 +1,50 @@
+package br.com.andreguedes.xti;
+
+/**
+ * Operators and operands
+ * @author Andre
+ *
+ */
+public class Aula13 {
+	// Aula 013 - Operadores
+	
+	public static void main(String[] args) {
+		// An expression, in which has operands and one or more operators
+		//2 + 3 : Binary operator
+		//-2 : Unary operator
+		//true ? "All right!!" : "What a pity!" : Ternary operator
+		
+		int x = 9 + 4; //= : Assignment operator
+		System.out.println(x);
+	
+		String s = "9" + "4"; // Concatenate
+		System.out.println(s);
+		
+		// Operators in Java
+		//
+		// +, -, *, /, %
+		//+
+		//+, -
+		//<, >, <=, >=
+		//++, --
+		//==, !=
+		//&&, ||
+		//.
+		//[]
+		//()
+		//?:
+		//<<, >>, >>>
+		//˜
+		//!
+		//&, |, ˆ
+		//,
+		//=
+		//+=, -=, *=, /=
+		//<<=, >>=, >>>=
+		//&=, |=, ˆ=
+		//instanceof
+		//new
+		//(type)
+	}
+
+}
