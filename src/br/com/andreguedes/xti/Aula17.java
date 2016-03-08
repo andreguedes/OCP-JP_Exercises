46ae0645265adcb62967bca25abc1882afb08e76
andresguedes <andresguedes@live.com>
2016-03-08 13:17:12 -0300

    Adding the Lesson 17 - Assignment operators

---
+++ src/br/com/andreguedes/xti/Aula17.java
@@ -0,0 +1,26 @@
+package br.com.andreguedes.xti;
+
+/**
+ * Assignment operator
+ * @author Andre
+ *
+ */
+public class Aula17 {
+	// Aula 017 - Operadores de atribuicao
+	
+	public static void main(String[] args) {
+		int x = 6;
+		x = x + 3;
+		x += 3;
+		
+		System.out.println(x);
+		
+		x *= 2;
+		x /= 2;
+		x -= 2;
+		x %= 2;
+		
+		System.out.println(x);
+	}
+
+}
