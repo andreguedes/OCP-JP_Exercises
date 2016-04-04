513e036d58628ffe64cbc84bd8fc47d8ed8853a5
andresguedes <andresguedes@live.com>
2016-04-04 13:31:24 -0300

    Adding the Lesson 18 - Special operators

---
+++ src/br/com/andreguedes/xti/Aula18.java
@@ -0,0 +1,20 @@
+package br.com.andreguedes.xti;
+
+/**
+ * Bitwise operators and special operators
+ * @author Andre
+ *
+ */
+public class Aula18 {
+	// Aula 018 - Operadores especiais
+	
+	public static void main(String[] args) {
+		int age = 6;
+		
+		// Ternary operation
+		String x = age >= 18 ? "Adult" : "Child";
+		
+		System.out.println(x);
+	}
+	
+}
